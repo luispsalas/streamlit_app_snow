d6494465c5bbfebab519b08d48fb989b50207db2
luispsalas <94636544+luispsalas@users.noreply.github.com>
2022-08-08 12:25:37 -0600

    Update streamlit_app.py
    added header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
+
 streamlit.title("Streamlit Title")
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
