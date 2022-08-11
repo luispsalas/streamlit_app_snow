cbcc19751801db0a1163aac87b6050a2cb9c4f46
luispsalas <94636544+luispsalas@users.noreply.github.com>
2022-08-11 17:47:51 -0600

    Update streamlit_app.py
    Changed json() into dataframe
    Changed url for response

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# Normalize json()
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Into a dataframe
+streamlit.dataframe(fruityvice_normalized)
