5cbcda138f593c05e1b14f59bd55c939ebd274a0
luispsalas <94636544+luispsalas@users.noreply.github.com>
2022-08-13 13:39:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
 
 # don't run anything until we troubleshoot:
-#streamlit.stop()
+streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 
